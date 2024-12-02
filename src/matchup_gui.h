b30e273b692976a1a8aa786586b2fe59c8d97f6b
Quinn <164554088+qgchambe@users.noreply.github.com>
2024-12-01 20:08:18 -0500

    Base code
    I'll be real some of this code was made on my pc and the rest was made on my laptop. i have not been able to get anything to compile on my laptop yet but was on my pc. so some of the code might have errors as ive yet to compile it in a while and mainly have just been going off online guides on gtk

---
+++ src/matchup_gui.h
@@ -0,0 +1,11 @@
+#ifndef MATCHUP_GUI_H
+#define MATCHUP_GUI_H
+
+#include <gtk/gtk.h>
+
+// Function declarations for the GUI
+void on_profile_edit_button_clicked(GtkButton *button, gpointer data);
+void on_search_button_clicked(GtkButton *button, gpointer data);
+void on_match_button_clicked(GtkButton *button, gpointer data);
+
+#endif // MATCHUP_GUI_H
